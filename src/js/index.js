561701a7420cb18c29317859fd2edd8e858e4ae2
Oleg Postnikov <olegspostnikov@gmail.com>
2017-09-07 15:47:20 +0500

    Верстка готова

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import '../scss/index.scss';
 import '../json/channels.json';
 import '../json/programms.json';
+
+
+/* Логика приложения */
+
+prepareDOM();
+
+
+/* Функции */
+function prepareDOM() {
+	// body...
+}
